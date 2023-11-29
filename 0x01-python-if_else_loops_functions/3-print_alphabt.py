47625fa8d5ca91c61888f9c127452424e3b52b55
Philip Ajuong Deng <philipajuong@gmail.com>
2023-11-28 18:23:50 -0800

    Task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-# Author - Philip Ajuong
-for i in range(97, 123): if i != 101 and i != 113:
+# Author - Philip Ajuong Deng
+for i in range(97, 123): 
+    if i != 101 and i != 113:
         print("{}".format(chr(i)), end="")

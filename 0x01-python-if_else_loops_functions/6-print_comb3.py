36e92f889a1e124b4226d304fc21f21645112220
Philip Ajuong Deng <philipajuong@gmail.com>
2023-11-29 04:10:33 -0800

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 # Author - Philip Ajuong
 for digit1 in range(0, 10):
-    for digit2 in range(digit1 + 1, 10):
-        if digit1  >= digit2:
-            continue
-        elif digit1 == 8 and digit2 == 9:
+    for digit2 in range(digit1 + 1, 10)
+        if digit1 == 8 and digit2 == 9:
             print("{}{}".format(digit1, digit2))
         else:
             print("{}{}, ".format(digit1, digit2), end="")

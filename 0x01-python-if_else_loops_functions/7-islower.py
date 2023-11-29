fabe654592f43ded9a3db736d8c6a450a2a348f5
Philip Ajuong Deng <philipajuong@gmail.com>
2023-11-29 04:41:36 -0800

    Task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+
+# Author - Philip Ajuong Deng
+
 def islower(c):
+    """function check for lowercase characters."""
     if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:

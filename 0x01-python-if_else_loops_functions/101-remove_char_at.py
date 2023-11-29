b642fac8beeb567e6c3bc768c1f5157a1f62372f
Philip Ajuong Deng <philipajuong@gmail.com>
2023-11-29 08:07:33 -0800

    Task 101

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Author - Philip Ajuong Deng
+
+"""Write a function that creates a copy of the string, removing
+        removing the character at the position n
+        (not the Python way, the “C array index”)."""
+
+def remove_char_at(str, n):
+    newStr = ""
+    for i in range(len(str)):
+        if i == n:
+            continue
+        else:
+            newStr += str[i]
+    return newStr

76ad2549c6b41252174882c07bf8d01b1869b3f0
Philip Ajuong Deng <philipajuong@gmail.com>
2023-11-29 08:24:44 -0800

    Task 102

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-#Author - Philip Ajuong Deng
+# Author - Philip Ajuong Deng
 
 """Write the Python function def magic_calculation(a, b, c)
     that does exactly the same as the Python bytecode does."""
+
 
 def magic_calculation(a, b, c):
     if a < b:

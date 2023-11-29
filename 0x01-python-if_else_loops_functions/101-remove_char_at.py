90e62f137259bed65708819474b45c813fcf0c37
Philip Ajuong Deng <philipajuong@gmail.com>
2023-11-29 08:12:37 -0800

    Task 101

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -5,6 +5,7 @@
         removing the character at the position n
         (not the Python way, the “C array index”)."""
 
+
 def remove_char_at(str, n):
     newStr = ""
     for i in range(len(str)):

98e00c5df4f1a0ffe875d81e505befe5cd1b43a2
Philip Ajuong Deng <philipajuong@gmail.com>
2023-11-29 05:46:15 -0800

    Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+# Author - Philip Ajuong Deng
+
 def uppercase(str):
-    for i in range(len(str)):
-        char = ord(str[i])
+    for uppercase in range(len(str)):
+
+        char = ord(str[uppercase])
         if char >= 97 and char <= 122:
             char = char - 32
         print("{}".format(chr(char)), end="")

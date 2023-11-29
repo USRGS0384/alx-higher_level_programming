2b42242454a8a28e0b4aca4a13247f24d3964893
Philip Ajuong Deng <philipajuong@gmail.com>
2023-11-28 18:39:38 -0800

    Task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # Author - Philip Ajuong Deng
 for i in range(97, 123):
-    if chr(letter) i != 101 and chr(letter) i != 113:
-         print("{}".format(chr(i)), end="")
+    if chr(letter) i != "a" and chr(letter)i != "z":
+        print("{}".format(chr(i)), end="")

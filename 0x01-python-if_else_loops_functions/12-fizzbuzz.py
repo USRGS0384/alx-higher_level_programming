bd0c6917a44512c604760e13fe9cb5279415275e
Philip Ajuong Deng <philipajuong@gmail.com>
2023-11-29 07:18:07 -0800

    Task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # Author - Philip Ajuong Deng
 
-"""prints the numbers from 1 to 100 separated by a space."""
+"""prints the numbers from 1 to 100 separated by a space
     For multiples of three, print Fizz instead of the number
-    on multiples of five, print Buzz instead of number
-    on both three and five, print FizzBuzz instaed number
+    for multiples of five, print Buzz instead of number
+    for both three and five, print FizzBuzz instaed number."""
 
 def fizzbuzz():
     for number in range(1, 101):

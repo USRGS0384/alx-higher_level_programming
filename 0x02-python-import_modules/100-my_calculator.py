e0b4b9aaf606f8000667f0a10da1c732cbb901a6
Philip Ajuong Deng <philipajuong@gmail.com>
2023-12-01 05:54:25 -0800

    Task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
 
-from sys import argv
-from calculator_1 import add, sub, mul, div
-if __name__ != "__main__":
-    exit()
+if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1
 
-argc = len(argv) - 1
-if argc != 3:
-    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
+op = argv[2]
+    num1 = int(argv[1])
+    num2 = int(argv[3])
+
+if op is '+':
+    print("{:d} + {:d} = {:d}".format(num1, num2, add(num1, num2)))
+    elif op is '-':
+    print("{:d} - {:d} = {:d}".format(num1, num2, sub(num1, num2)))
+    elif op is '*':
+    print("{:d} * {:d} = {:d}".format(num1, num2, mul(num1, num2)))
+    elif op is '/':
+    print("{:d} / {:d} = {:d}".format(num1, num2, div(num1, num2)))
+    else:
+    print("Unknown operator. Available operators: +, -, * and /")
     exit(1)
-elif argv[2] == '+':
-    func = add
-elif argv[2] == '-':
-    func = sub
-elif argv[2] == '*':
-    func = mul
-elif argv[2] == '/':
-    func = div
-else:
-    print("Unknown operator. Available operators: +, -, *, and /")
-    exit(1)
-
-result = func(int(argv[1]), int(argv[3]))
-print("{:d} {:s} {:d} = {:d}".format(int(argv[1]),
-    argv[2], int(argv[3]), result))

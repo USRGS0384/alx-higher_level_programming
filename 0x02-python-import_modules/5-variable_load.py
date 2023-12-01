f22b044be018e231f589a1c91b139200dccaf926
Philip Ajuong Deng <philipajuong@gmail.com>
2023-12-01 05:30:05 -0800

    Task 5

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-#Author - Philip Ajuong Deng
-from variable_load_5 import a
-if __name__ != "__main__":
-    exit()
-
-print(f"{:d}" a)
+if __name__ == "__main__":
+    from variable_load_5 import a
+    print(a)

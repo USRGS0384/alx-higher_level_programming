dcd1f11513169377aef399f01651bca78be57569
Philip Ajuong Deng <philipajuong@gmail.com>
2023-12-02 13:57:33 -0800

    Task 7

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-exec(chr(112)+chr(114)+chr(105)+chr(110)+chr(116)+chr(40)+chr(34)+chr(35)+chr(112)+chr(121)+chr(116)+chr(104)+chr(111)+chr(110)+chr(105)+chr(115)+chr(99)+chr(111)+chr(111)+chr(108)+chr(34)+chr(41))
+builtins.__dict__['_'*6](_='_', __='pythoniscool')

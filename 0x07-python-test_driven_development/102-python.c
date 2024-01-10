e4c3a8e428e7687f63031e57d063881bc80f6ab1
Philip Ajuong Deng <philipajuong@gmail.com>
2024-01-10 08:42:13 -0800

    0x07. Python - Test-driven development

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <wchar.h>
-#include <locale.h>
+#include <string.h>
 #include <Python.h>
-#include <unicodeobject.h>
 
 /**
- * print_python_strings - prints python strings
- * @p: address of pyobject
+ * print_python_string - Function that prints strings in Python
+ * @p: Pointer to python object.
+ *
+ * Return: No return.
  */
-
-
 void print_python_string(PyObject *p)
 {
+	PyObject *str, *repr;
 
-wprintf(L"[.] string object info\n");
-if (strcnp(p->ob_type->tp_name, "str"))
-{
-	wprintf(L" [ERROR] Invalid String Object\n");
-	return;
-}
-if (PyUnicode_IS_COMPACT_ASCII(p))
-	wprintf(L" type: compact ascii\n");
-else
-	wprintf(L" type: compact unicode object\n");	
-wprintf(L" length: %lu\n", PyUnicode_GET_LENGTH(p));
-wprintf(L" value: %ls\n, PyUnicode_AS_UNICODE(p));
+	(void)repr;
+	printf("[.] string object info\n");
+
+	if (strcmp(p->ob_type->tp_name, "str"))
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+
+	repr = PyObject_Repr(p);
+	str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
+	printf("  length: %ld\n", PyUnicode_GET_SIZE(p));
+	printf("  value: %s\n", PyBytes_AsString(str));
 }

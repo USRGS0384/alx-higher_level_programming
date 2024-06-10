e79462b9e493bc36a05cbd074e64af5b7bced053
Philip Ajuong Deng <philipajuong@gmail.com>
2024-06-10 18:49:11 +0200

    Python everything almost curcle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -155,11 +155,11 @@ class Base:
             list_squares (list): A list of Square objects to draw.
         """
         turt = turtle.Turtle()
-        turt.screen.bgcolor("#b7312c")
+        turt.screen.bgcolor("#E0FFFF")
         turt.pensize(3)
         turt.shape("turtle")
 
-        turt.color("#ffffff")
+        turt.color("#0000FF")
         for rect in list_rectangles:
             turt.showturtle()
             turt.up()
@@ -172,7 +172,7 @@ class Base:
                 turt.left(90)
             turt.hideturtle()
 
-        turt.color("#b5e3d8")
+        turt.color("#00FF00")
         for sq in list_squares:
             turt.showturtle()
             turt.up()

e1b68f908ad00c4a5d5d0c1a7d5187d83295b109
Philip Ajuong Deng <philipajuong@gmail.com>
2024-07-05 08:20:05 +0300

    0x10. Python - Network #0

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# write a script that dispaly a body of content
-curl -ls "$1"
+# Write a script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -Ls "$1"

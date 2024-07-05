b71fb10fdcbc263ff75218bee50f17dc757e0e24
Philip Ajuong Deng <philipajuong@gmail.com>
2024-07-05 23:32:16 +0300

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,7 +13,7 @@ def find_peak(list_of_integers):
     peak = None
     if (list_of_integers[0] >= list_of_integers[1]):
         peak = list_of_integers[0]
-    if (list_of_integers[-1] >= list_of_integers[-2]):
+    if (list_of_integers[-1] >= list_of_integers[2]):
         peak = list_of_integers[-1]
     if (peak):
         return peak

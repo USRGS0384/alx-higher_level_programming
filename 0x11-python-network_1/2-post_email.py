237444b6cef96c07aa42926d2b16663c79934700
Philip Ajuong Deng <philipajuong@gmail.com>
2024-07-06 01:05:44 +0300

    0x11. Python - Network #1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL
-- sends a POST request to the passed URL
-- takes email as a parameter
-- displays the body of the response
 """
-import sys
-import urllib.parse
-import urllib.request
+Script that takes in a URL and an email, sends a POST request to the passed
+URL with the email as a parameter.
+"""
+from sys import argv
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    url = argv[1]
+    value = {"email": argv[2]}
+    data = urlencode(value).encode("ascii")
+    req = Request(url, data)
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    with urlopen(req) as response:
+        print(response.read().decode("utf-8", "replace"))
